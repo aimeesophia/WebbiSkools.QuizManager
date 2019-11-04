9c4120321e4bbd8b94dbc6a33538fdd49f0b769f
Aimee Craig <aimee.craig@bglgroup.co.uk>
2019-11-04 16:08:52 +0000

    Created Login view.

---
+++ WebbiSkools.QuizManager.Web/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebbiSkools.QuizManager.Web.Data;
+
+namespace WebbiSkools.QuizManager.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly QuizManagerContext _context;
+
+        public AccountController(QuizManagerContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
