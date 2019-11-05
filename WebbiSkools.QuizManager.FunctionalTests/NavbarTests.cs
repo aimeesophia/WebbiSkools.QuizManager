542490b1b116658359922ca32b53ecbe2d4a74be
Aimee Craig <aimee.craig@bglgroup.co.uk>
2019-11-05 15:44:39 +0000

    Moved navbar tests to separate file.

---
+++ WebbiSkools.QuizManager.FunctionalTests/NavbarTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebbiSkools.QuizManager.FunctionalTests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class NavbarTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Test]
+        public void Navbar_When_User_Is_Authenticated_Shows_Welcome_Message()
+        {
+            // Arrange
+            var expected = "Hi, RestrictedPermissionsUser!";
+            TestHelper.Login("RestrictedPermissionsUser", _driver);
+
+            // Act
+            var actual = _driver.FindElement(By.CssSelector("[data-testid='navbar-welcome-message']"));
+
+            // Assert
+            Assert.AreEqual(expected, actual.Text);
+        }
+
+        [Test]
+        public void Navbar_When_User_Signs_Out_Removes_Welcome_Message()
+        {
+            // Arrange
+            TestHelper.Login("RestrictedPermissionsUser", _driver);
+
+            // Act
+            _driver.FindElement(By.CssSelector("[data-testid='navbar-logout-button']")).Click();
+            var navbarWelcomeMessageExists = TestHelper.ElementExists(By.CssSelector("[data-testid='navbar-welcome-message']"), _driver);
+
+            // Assert
+            Assert.IsFalse(navbarWelcomeMessageExists);
+        }
+    }
+}
