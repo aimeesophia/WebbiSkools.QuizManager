4ef0bf32f12d5e6432f94cde99b85ea19ff0b31f
Aimee Craig <aimee.craig@bglgroup.co.uk>
2019-11-08 17:12:53 +0000

    Removed comments.

--- WebbiSkools.QuizManager.Web/Startup.cs
+++ WebbiSkools.QuizManager.Web/Startup.cs
@@ -20,7 +20,6 @@ namespace WebbiSkools.QuizManager.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
@@ -39,7 +38,6 @@ namespace WebbiSkools.QuizManager.Web
                 options.UseSqlServer(Configuration.GetConnectionString("TestConnection")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -49,7 +47,6 @@ namespace WebbiSkools.QuizManager.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
